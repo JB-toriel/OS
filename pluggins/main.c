d410a26b1508930cc7b9173f69d9f430ca5d16f8
JB-toriel <eddejb@gmail.com>
2020-02-29 16:33:27 +0100

    nothing

--- pluggins/main.c
+++ pluggins/main.c
@@ -38,12 +38,6 @@ int main( int argc , char* argv[] ) {
       }
     }*/
 
-    if ( argc>1 ){
-      if ( !strcmp( argv[1], "hello" ) ){
-          hello_plug();
-      }
-    }
-
     dlclose(handle);
 
     exit(EXIT_SUCCESS);
